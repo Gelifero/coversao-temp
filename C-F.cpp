34ee28b180e3cfb784496dcca005fb41919a52cc
ra2457048 <ra2457048@MATRIZDELLMECAN>
2025-03-17 19:47:18 -0300

    C-K

--- C-F.cpp
+++ C-F.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-float celsius;//temp
-float fahrenheit;
+float celsius;
+float kelvin;
 printf("Quanto esta medindo a temperatura em graus celsius:");
 scanf("%f", &celsius);
-fahrenheit = (celsius*9/5) +32;
-printf("Sua temperatura em fahrenheit e %2.f", fahrenheit);
+kelvin = celsius + 273,15 
+printf("Sua temperatura em kelvin e %2.f", kelvin);
 }
